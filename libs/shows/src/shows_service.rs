a7ec25c413e22c10fb67aac14cda996187879075
Brandon Konkle <brandon@konkle.us>
2021-11-16 18:09:46 -0700

    Clean up the refs a bit

--- libs/shows/src/shows_service.rs
+++ libs/shows/src/shows_service.rs
@@ -48,18 +48,19 @@ mod tests {
     #[tokio::test]
     async fn test_get_show() {
         let show = create_show();
+        let response = show.clone();
 
         let mut shows_repo = MockShowsRepository::new();
 
         shows_repo
             .expect_get()
             .times(1)
-            .with(eq(show.id))
-            .returning(|_| Ok(Some(show)));
+            .with(eq(String::from(&show.id)))
+            .returning(move |_| Ok(Some(response.clone())));
 
         let service = ShowsService::new(&Arc::new(shows_repo));
 
-        let result = service.get(show.id).await;
+        let result = service.get(String::from(&show.id)).await;
 
         match result {
             Ok(result_opt) => match result_opt {
